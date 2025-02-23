f58e72eed947ff23185df8704393edd40fb67019
ivan <ivann97n@gmail.com>
2025-02-23 15:16:50 +0300

    feat: make IngredientDetails component

--- src/components/burger-ingredients/ingredient-details/ingredient-details.jsx
+++ src/components/burger-ingredients/ingredient-details/ingredient-details.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 
-export default function IngredientDetails() {
-	return <></>;
+export default function IngredientDetails(props) {
+	return (
+		<>
+			<div>
+				<p>{props.price}</p>
+				<CurrencyIcon />
+			</div>
+			<p>{props.name}</p>
+		</>
+	);
 }
