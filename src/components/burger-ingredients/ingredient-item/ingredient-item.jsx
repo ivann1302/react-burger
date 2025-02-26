349b5757bc9ed8d325855e34b342772875cabd18
ivan <ivann97n@gmail.com>
2025-02-26 14:00:13 +0300

    feat: add Counter for IngredientItem component

--- src/components/burger-ingredients/ingredient-item/ingredient-item.jsx
+++ src/components/burger-ingredients/ingredient-item/ingredient-item.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Counter } from '@ya.praktikum/react-developer-burger-ui-components';
 
 export default function IngredientItem(props) {
 	return (
 		<>
+			<Counter count={1} size='default' extraClass='m-1' />
 			<img src={props.img} alt='Ingredient' />
 		</>
 	);
