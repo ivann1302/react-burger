ffc2d2eb90085ec3033aa373f171354c76da17de
ivan <ivann97n@gmail.com>
2025-03-06 09:53:47 +0300

    feat: add tabs scroll functional in BurgerIngredients

--- src/components/burger-ingredients/ingredients-group/ingredients-group.jsx
+++ src/components/burger-ingredients/ingredients-group/ingredients-group.jsx
@@ -1,33 +1,34 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import IngredientItem from './../ingredient-item/ingredient-item';
 import IngredientInformation from '../ingredient-information//ingredient-information';
 import styles from './ingredients-group.module.scss';
 
-export default function IngredientsGroup({
-	type,
-	groupName,
-	onIngredientClick,
-}) {
-	return (
-		<>
-			<h3 className={`${styles.title} ext text_type_main-medium`}>
-				{groupName}
-			</h3>
-			<ul className={styles.gridContainer}>
-				{type.map((ingredient) => (
-					// eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions
-					<li
-						key={ingredient._id}
-						className={styles.container}
-						onClick={() => onIngredientClick(ingredient)}>
-						<IngredientItem img={ingredient.image} />
-						<IngredientInformation
-							name={ingredient.name}
-							price={ingredient.price}
-						/>
-					</li>
-				))}
-			</ul>
-		</>
-	);
-}
+// eslint-disable-next-line react/display-name
+const IngredientsGroup = forwardRef(
+	({ type, groupName, onIngredientClick }, ref) => {
+		return (
+			<>
+				<h3 ref={ref} className={`${styles.title} text text_type_main-medium`}>
+					{groupName}
+				</h3>
+				<ul className={styles.gridContainer}>
+					{type.map((ingredient) => (
+						// eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions
+						<li
+							key={ingredient._id}
+							className={styles.container}
+							onClick={() => onIngredientClick(ingredient)}>
+							<IngredientItem img={ingredient.image} />
+							<IngredientInformation
+								name={ingredient.name}
+								price={ingredient.price}
+							/>
+						</li>
+					))}
+				</ul>
+			</>
+		);
+	}
+);
+
+export default IngredientsGroup;
