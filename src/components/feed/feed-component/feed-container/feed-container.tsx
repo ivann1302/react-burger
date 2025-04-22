1065c551348891f571d2783f736d523979840fb4
ivan <ivann97n@gmail.com>
2025-04-22 20:21:19 +0300

    feat: add react logic with test data in feed component

--- src/components/feed/feed-component/feed-container/feed-container.tsx
+++ src/components/feed/feed-component/feed-container/feed-container.tsx
@@ -1,9 +1,30 @@
 import FeedElement from '../feed-element/feed-element';
+import ingredients from './../../../../utils/data';
+import style from './feed-container.module.scss';
 const FeedContainer = () => {
 	return (
-		<>
-			<FeedElement></FeedElement>
-		</>
+		<div className={`${style.container}`}>
+			<FeedElement
+				name={'Burger'}
+				number={'12315'}
+				ingredients={ingredients}
+				price={480}></FeedElement>
+			<FeedElement
+				name={'Burger'}
+				number={'12315'}
+				ingredients={ingredients}
+				price={480}></FeedElement>
+			<FeedElement
+				name={'Burger'}
+				number={'12315'}
+				ingredients={ingredients}
+				price={480}></FeedElement>
+			<FeedElement
+				name={'Burger'}
+				number={'12315'}
+				ingredients={ingredients}
+				price={480}></FeedElement>
+		</div>
 	);
 };
 
--- src/components/feed/feed-component/feed-element/feed-element.tsx
+++ src/components/feed/feed-component/feed-element/feed-element.tsx
@@ -1,17 +1,69 @@
 import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './feed-element.module.scss';
-const FeedElement = () => {
+
+type TFeedElement = {
+	name: string;
+	number: string;
+	ingredients: any[];
+	price: number;
+	date?: string;
+};
+
+const FeedElement = ({
+	name,
+	number,
+	ingredients,
+	price,
+	date = 'Сегодня, 16:20',
+}: TFeedElement) => {
+	// Функция для отображения ингредиентов
+	const renderIngredients = () => {
+		const visibleIngredients = ingredients.slice(0, 6);
+		const remainingCount = ingredients.length - 6;
+
+		return (
+			<div className={styles.ingredientsContainer}>
+				{visibleIngredients.map((ingredient, index) => (
+					<div
+						key={`${ingredient._id}-${index}`}
+						className={styles.ingredientWrapper}
+						style={{ zIndex: visibleIngredients.length - index }}>
+						<img
+							src={ingredient.image}
+							alt={ingredient.name}
+							className={styles.ingredientImage}
+						/>
+						{index === 5 && remainingCount > 0 && (
+							<div
+								className={`${styles.remainingCounter} text text_type_main-default`}>
+								+{remainingCount}
+							</div>
+						)}
+					</div>
+				))}
+			</div>
+		);
+	};
+
+	// Вычисляем общую стоимость
+	const totalPrice =
+		price || ingredients.reduce((sum, ing) => sum + ing.price, 0);
+
 	return (
 		<div className={`${styles.container} p-6`}>
-			<div className={`${styles.firstBlock}`}>
-				<h3 className='text text_type_digits-default'>#034535 </h3>
-				<p className='text text_type_main-small'>Сегодня, 16:20</p>
+			<div className={styles.header}>
+				<span className='text text_type_digits-default'>#{number}</span>
+				<span className='text text_type_main-default text_color_inactive'>
+					{date}
+				</span>
 			</div>
-			<h3 className='text text_type_main-medium'>Starship main burger</h3>
-			<div className={styles.secondBlock}>
-				<div className={styles.order}>Order Image</div>
-				<div className={styles.orderPrice}>
-					<p className='text text_type_digits-default'>480</p>
+
+			<h3 className={`${styles.title} text text_type_main-medium`}>{name}</h3>
+
+			<div className={styles.footer}>
+				<div className={styles.ingredients}>{renderIngredients()}</div>
+				<div className={styles.price}>
+					<span className='text text_type_digits-default'>{totalPrice}</span>
 					<CurrencyIcon type='primary' />
 				</div>
 			</div>
