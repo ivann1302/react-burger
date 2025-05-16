2eef991a7dfc8bb26055095bfe710d44187da457
ivan <ivann97n@gmail.com>
2025-05-16 07:53:18 +0300

    fix: fix bugs

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
 	e2e: {
 		baseUrl: 'http://localhost:8080',
 		supportFile: 'cypress/support/e2e.ts',
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
 		setupNodeEvents(on, config) {
 			// implement node event listeners here
 		},
